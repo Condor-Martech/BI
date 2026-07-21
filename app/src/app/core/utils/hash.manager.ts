34cd4968f4d29008cd05f37c5c7a02720ca4c257
Héctor Rodríguez <rodriguez.hectoralejandro@gmail.com>
2026-07-20 22:13:54 -0300

    fix(auth): use crypto.randomInt for password generation

--- app/src/app/core/utils/hash.manager.ts
+++ app/src/app/core/utils/hash.manager.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import * as bcrypt from "bcryptjs";
+import { randomInt } from "crypto";
 
 @Injectable()
 export class HashManager {
@@ -14,14 +15,11 @@ export class HashManager {
     }
     public generatePassword(length: number = 8): string {
         const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-        let password = '';
-
+        let result = '';
         for (let i = 0; i < length; i++) {
-            const randomIndex = Math.floor(Math.random() * characters.length);
-            password += characters.charAt(randomIndex);
+            result += characters[randomInt(0, characters.length)];
         }
-
-        return password;
+        return result;
     }
 }
 
